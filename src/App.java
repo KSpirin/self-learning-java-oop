50e6804cce1c4933944483a01b4f28145420f663
KSpirin <thanakornkb315@gmail.com>
2026-04-25 17:31:26 +0700

    datatype

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int x = 10;
-        double y = 1.2;
-        int mul = (int)(x * y); //casting to int
-        System.out.println("result: " + mul);
+        //DATATYPE
+        int age = 19;
+        long salary = 9000000000000L;
+
+        double pi = 3.1415926535;
+        float gpax = 3.12f;
+
+        char grade = 'A';
+        String name = "Miya";
+
+        boolean liar = false;
+
+        System.out.println(name + " is " + age + " years old\n");
     }
 }
