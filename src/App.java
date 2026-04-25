462d100cc69d5ff89e2a151e63a1e995f7d08aa9
KSpirin <thanakornkb315@gmail.com>
2026-04-25 14:30:04 +0700

    first input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("first input: " + args[0]);
+        System.out.println("first input: " + args[1]);
     }
 }
