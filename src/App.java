692a22945de946efe90032e9630c10062c3d3adf
KSpirin <thanakornkb315@gmail.com>
2026-04-27 19:48:00 +0700

    array

--- src/App.java
+++ src/App.java
@@ -2,15 +2,26 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
+        //method 1
+        System.out.println("enter 5 number");
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++)
+            arr[i] = input.nextInt();
+        for (int i = 0; i < 5; i++)
+            System.out.print(arr[i] + " ");
+        System.out.print("\n");
+        //method 2
+        String[] name = {"miya","captain","tar"};
 
-        System.out.print("enter n: ");
-        int n = input.nextInt();
+        //sizeof
+        System.out.println("size of name = " + name.length);
 
-        for(int i = 0; i < n; i++)
-            System.out.println(i+1);
-
-        input.close();
+        // For-Each Loops
+        for(String n : name)
+            System.out.print(n + " ");
     }
 
 }
 // System.out.println();
+// Scanner input = new Scanner(System.in);
+//intput.close()
