43efa719c546bf4458453a11760628894e9d13f5
KSpirin <thanakornkb315@gmail.com>
2026-04-27 21:00:13 +0700

    Function(method) : array(void)

--- src/App.java
+++ src/App.java
@@ -2,24 +2,31 @@ import java.util.Scanner;
 
 public class App {
 
-    public static double calvat(double price) {// int double
-        double finalprice = price * (0.07);
-        return finalprice;
+    public static void caldiscount(double arr[],int Dis) { // array
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = arr[i] * (1 - Dis / 100.0); // discount 50% off
+        //if we use / 100 its will be int and make this code error
+        // 50 / 100 = 0 NOT 0.5 bc int thats why
     }
 
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("enter the price: ");
-        int price = in.nextInt();
-        double vat = calvat(price);
-        System.out.printf("vat 7%% = %.2f\n", calvat(price)); //"%%" = %
-        System.out.printf("current price = %.2f\n", (price + vat));
+        double[] price = {130, 70, 75, 50, 123};
+        for (double p : price)
+            System.out.printf("%.2f ",p);
 
-        in.close();
+        System.out.print("\nendter the discount(%): ");
+        int discount = input.nextInt();
+        caldiscount(price, discount);
+
+        for (double p : price)
+            System.out.printf("%.2f ", p);
+
+        input.close();
     }
 
 }
 // System.out.println();
 // Scanner input = new Scanner(System.in);
-// intput.close();
+// input.close();
